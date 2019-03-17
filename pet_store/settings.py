3f181cd9d51a4a011eb3d0d80bff160a0fda99f2
ipaullly <ipaully22@gmail.com>
2019-03-17 14:56:07 +0300

    add environment variables

--- pet_store/settings.py
+++ pet_store/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import environ
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
