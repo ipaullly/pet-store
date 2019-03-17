917c4ecce5fd930108e32d4751f13a4fb889ccbd
ipaullly <ipaully22@gmail.com>
2019-03-17 22:22:41 +0300

    Add failing tests for POST view

--- pets/tests.py
+++ pets/tests.py
@@ -1,7 +1,7 @@
 import json
 from django.test import TestCase, Client
 from django.urls import reverse
-from .serializers PuppySerializer
+from .serializers import PetSerializer
 from .models import Pet
 from rest_framework import status
 
@@ -26,3 +26,34 @@ class PetTest(TestCase):
         self.assertEqual(
             pet_judas.get_animal_type(), "Judas is a West African bush viper."
         )
+
+class CreateNewPetTest(TestCase):
+    """Test module for inserting a new puppy"""
+    def setUp(self):
+        self.valid_payload = {
+            'name': 'Muffin',
+            'age': 4,
+            'animal_type': 'Namibian grass snake',
+            'color': 'Bronze'
+        }
+        self.invalid_payload = {
+            'name': '',
+            'age': 4,
+            'animal_type': 'Dwark Hamster',
+            'color': 'White'
+        }
+    def test_create_valid_pet(self):
+        response = client.post(
+            reverse('get_post_pets'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_create_invalid_pet(self):
+        response = client.post(
+            reverse('get_post_pets'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
