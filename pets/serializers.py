f71ba73f4bd1e0d0d6e522c4b42b8189406078b1
ipaullly <ipaully22@gmail.com>
2019-05-25 14:18:46 +0300

    edit serializer to return id

--- pets/serializers.py
+++ pets/serializers.py
@@ -4,4 +4,4 @@ from .models import Pet
 class PetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pet
-        fields = ('name', 'age', 'animal_type', 'color', 'created_at', 'updated_at')
+        fields = ('name', 'age', 'animal_type', 'color', 'id')
